81e2f4c12e8700fa9282b5b63ced57fbeffd6433
Adam Ammour <adammour@paul-f3Br10s1.clusters.42paris.fr>
2025-01-27 20:39:02 +0100

    norminette ok + function to check

--- get_next_line.c
+++ get_next_line.c
@@ -3,132 +3,131 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adammour <skn.aga108@gmail.com>            +#+  +:+       +#+        */
+/*   By: adammour <adammour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 21:17:10 by adammour          #+#    #+#             */
-/*   Updated: 2025/01/27 18:45:21 by adammour         ###   ########.fr       */
+/*   Updated: 2025/01/27 20:38:39 by adammour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char    *read_line(int fd, char *str)
+char	*ft_check(char *str)
 {
-    char    *buffer;
-    char    *new_str;
-    int rd;
-
-    rd = 1;
-    buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
-    if (!buffer)
-        return NULL;
-    if (!str)
-    {
-        str = malloc(sizeof(char));
-        if (!str)
-            return(free(buffer), NULL);
-        str[0] = '\0'; 
-    }
-    while (rd > 0 && !ft_strrchr(str, '\n'))
-    {
-        rd = read(fd, buffer, BUFFER_SIZE);
-        if (rd < 0)
-            return(free(buffer), NULL);
-        buffer[rd] = '\0';
-        new_str = ft_strjoin(str, buffer);
-        if (!new_str)
-            return(free(str), free(buffer), NULL); 
-        str = new_str;
-    }
-    free(buffer);
-    return (str);
+	if (!str)
+	{
+		str = malloc(sizeof(char));
+		if (!str)
+			return (NULL);
+		str[0] = '\0';
+	}
+	return (str);
 }
 
-char    *get_line(char *str)
+char	*read_line(int fd, char *str)
 {
-    char    *buffer;
-    int     i;
-    int     j;
+	char	*buffer;
+	char	*new_str;
+	int		rd;
 
-    i = 0;
-    if(!str)
-        return(NULL);
-    while (str[i] && str[i] != '\0')
-        i++;
-    buffer = malloc(sizeof(char) * (i + 2)); // +2 pour recup '\n' et '\0'
-    if (!buffer)
-        return(free(str), NULL);
-    j = 0;
-    while (j < i)
-    {
-        buffer[j] = str[j];
-        j++;
-    }
-    if (str[j] == '\n')
-        buffer[j++] = '\n';
-    buffer[j] = '\0';
-    return (buffer);
+	rd = 1;
+	buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	str = ft_check(str);
+	if (!str)
+		return (free(buffer), NULL);
+	while (rd > 0 && !ft_strrchr(str, '\n'))
+	{
+		rd = read(fd, buffer, BUFFER_SIZE);
+		if (rd < 0)
+			return (free(buffer), NULL);
+		buffer[rd] = '\0';
+		new_str = ft_strjoin(str, buffer);
+		if (!new_str)
+			return (free(str), free(buffer), NULL);
+		str = new_str;
+	}
+	free(buffer);
+	return (str);
 }
 
-char    *clean_line(char *str)
+char	*get_line(char *str)
 {
-    char    *buffer;
-    int i;
-    int j;
+	char	*buffer;
+	int		i;
+	int		j;
 
-    i = 0;
-    while (str[i] && str[i] != '\n')
-        i++;
-    if (!str)
-        return (free(str), NULL); // si str echoue libere memoire du 1er malloc
-    buffer = malloc(ft_strlen(&str[i]) + 1);
-    if(!buffer)
-        return (free(str), NULL);
-    j = 0;
-    while(str[i])
-        buffer[j++] = str[i++];
-    buffer[j]= '\0';
-    free(str);
-    return(buffer);
+	i = 0;
+	if (!str)
+		return (NULL);
+	while (str[i] && str[i] != '\0')
+		i++;
+	buffer = malloc(sizeof(char) * (i + 2));
+	if (!buffer)
+		return (free(str), NULL);
+	j = 0;
+	while (j < i)
+	{
+		buffer[j] = str[j];
+		j++;
+	}
+	if (str[j] == '\n')
+		buffer[j++] = '\n';
+	buffer[j] = '\0';
+	return (buffer);
 }
-char    *get_next_line(int fd)
+
+char	*clean_line(char *str)
 {
-    static char *buffer;
-    char *line;
+	char	*buffer;
+	int		i;
+	int		j;
 
-    if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
-        return (NULL);
-    buffer = read_line(fd, line);
-    if (!buffer)
-        return(NULL);
-    line = get_line(buffer);
-    buffer = clean_line(buffer);
-    return(line);
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (!str)
+		return (free(str), NULL);
+	buffer = malloc(ft_strlen(&str[i]) + 1);
+	if (!buffer)
+		return (free(str), NULL);
+	j = 0;
+	while (str[i])
+		buffer[j++] = str[i++];
+	buffer[j] = '\0';
+	free(str);
+	return (buffer);
 }
-//malloc la ligne qu'on return a la fin
-//parcourir la chaine de caractere jusqu'au \n et copier les caracteres
-//return la ligne 
-//verifier si il y a un \n a la fin de la ligne si oui l'ajouter
 
-//char    *clean_line(char *str)
-//malloc la ligne qui commence au \n et termine a la fin de read_line(fd, str)
-//parcourir la chaine de caractere a partir du \n et la copier dans la nouvelle str
-//return la chaine de caracteres
+char	*get_next_line(int fd)
+{
+	static char	*buffer;
+	char		*line;
 
-#include <stdio.h>
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+		return (NULL);
+	buffer = read_line(fd, line);
+	if (!buffer)
+		return (NULL);
+	line = get_line(buffer);
+	buffer = clean_line(buffer);
+	return (line);
+}
+
 #include <fcntl.h>
+#include <stdio.h>
 
-int main()
+int	main(void)
 {
-    int fd;
-    char *line;
+	int		fd;
+	char	*line;
 
-    fd = open("texte", O_RDONLY);
-    while ((line = get_next_line(fd)) != NULL)
-    {
-        printf("%s", line);
-        free(line);
-    }
-    return 0;
+	fd = open("texte", O_RDONLY);
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		printf("%s", line);
+		free(line);
+	}
+	return (0);
 }
-
--- get_next_line.h
+++ get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adammour <skn.aga108@gmail.com>            +#+  +:+       +#+        */
+/*   By: adammour <adammour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 21:23:36 by adammour          #+#    #+#             */
-/*   Updated: 2025/01/27 18:44:04 by adammour         ###   ########.fr       */
+/*   Updated: 2025/01/27 20:24:37 by adammour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 # define GET_NEXT_LINE_H
 # define BUFFER_SIZE 999
 
+# include <fcntl.h>
 # include <stddef.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include <fcntl.h>
 
-char    *read_line(int fd, char *str);
+char	*read_line(int fd, char *str);
 
-char    *get_line(char *str);
+char	*get_line(char *str);
 
-char    *clean_line(char *str);
+char	*clean_line(char *str);
 
-char    *get_next_line(int fd);
+char	*get_next_line(int fd);
 
 size_t	ft_strlen(const char *s);
 
